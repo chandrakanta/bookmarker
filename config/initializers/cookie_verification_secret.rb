3f31f5787638ec572f1b5ede2b2d8ea4e3a62afe
chandrakanta <soft.bbsr@andolasoft.com>
2010-09-15 11:01:26 +0530

    Signed-off-by: chandrakanta <soft.bbsr@andolasoft.com>

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '70cb7f7ade6d48b315af47aff458750033420d97f4c2ebba2a93a33395e9dde6f52564793f7d5d60f004ea629851c101d0fff47474c2ca0f6447b3a7b802b9ed';
